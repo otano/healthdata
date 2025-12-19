17e5d322e054179a1446d84822ab14994cf4750a
otano <laurentmattle@gmail.com>
2025-12-19 10:41:00 +0100

    uv conf

--- src/main.py
+++ src/main.py
@@ -2,6 +2,5 @@ import pandas as pd
 
 # Load dataset
 df = pd.read_csv(
-    "/kaggle/input/social-media-mental-health-indicators-dataset/mental_health_social_media_dataset.csv"
+    "../social-media-mental-health-indicators-dataset/mental_health_social_media_dataset.csv"
 )
-print("Original shape:", df.shape)
