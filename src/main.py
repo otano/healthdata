8502ea33dd58e5c91eef12908ce5bb1ced820964
otano <laurentmattle@gmail.com>
2026-01-04 22:08:12 +0100

    resolution origin main>main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
 import pandas as pd
 
 # Load dataset
-df = pd.read_csv(
-    "../social-media-mental-health-indicators-dataset/mental_health_social_media_dataset.csv"
-)
+df = pd.read_csv("mental_health_social_media_dataset.csv")
